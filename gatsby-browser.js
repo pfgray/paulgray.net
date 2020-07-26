ccd0f029422cd2ccfa0cd6082c0106d71b3e8677
Paul Gray <pfbgray@gmail.com>
2020-07-26 14:54:28 -0400

    changing colors

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
-
 const ReactGA = require("react-ga");
-require("./src/css/prism_theme.scss");
+require("./src/css/prism_theme_light.scss");
 
 ReactGA.initialize("UA-60737789-1");
 
@@ -9,8 +8,8 @@ exports.onRouteUpdate = (state, page, pages) => {
   ReactGA.pageview(state.location.pathname);
 };
 
-exports.onClientEntry = function() {
-  require('es6-object-assign').polyfill();
+exports.onClientEntry = function () {
+  require("es6-object-assign").polyfill();
 };
 
 // require("prism-themes/themes/prism-duotone-sea.css")
